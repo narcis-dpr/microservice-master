eec69ddc16fb70701ba8924bae52999cc5b3b305
narges-dpr <nargesdeypir@gmail.com>
2023-11-27 16:02:37 +0330

    Rest Api basic Controller

--- src/main/kotlin/com/microservice/master/controller/RestApiController.kt
+++ src/main/kotlin/com/microservice/master/controller/RestApiController.kt
@@ -1,8 +1,12 @@
 package com.microservice.master.controller
 
 import com.microservice.master.Coffee
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -37,5 +41,28 @@ class RestApiController {
                 return Optional.of(coffee)
         }
         return Optional.empty()
+    }
+
+    @PostMapping("/coffees")
+    fun postCoffee(@RequestBody coffee: Coffee): Coffee {
+        coffees.add(coffee)
+        return coffee
+    }
+
+    @PutMapping("/coffees/{id}")
+    fun putCoffee(@PathVariable id: String, @RequestBody coffee: Coffee): Coffee {
+        var coffeeIndex = -1
+        for (cf in coffees) {
+            if (cf.id == id) {
+                coffeeIndex = coffees.indexOf(cf)
+                coffees[coffeeIndex] = cf
+            }
+        }
+        return if (coffeeIndex == -1) postCoffee(coffee) else coffee
+    }
+
+    @DeleteMapping("/coffees/{id}")
+    fun deleteCoffee(@PathVariable id: String) {
+        coffees.removeIf { coffee -> coffee.id == id }
     }
 }
